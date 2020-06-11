40deda90b0a1872115013c9d9c7a3efb4e991664
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-06-11 14:19:27 -0300

    Gerando token

---
+++ shop/Controllers/UserController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shop.Data;
+using shop.Models;
+using shop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop.Controllers
+{
+    [Route("users")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<List<User>>> Get(
+            [FromServices] DataContext context)
+        {
+            var users = await context.Users.AsNoTracking().ToListAsync();
+            return users;
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<User>> Put(
+            [FromServices] DataContext context,
+            int id,
+            [FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (id != model.Id)
+                return NotFound(new { message = "Usuário não encontrado" });
+
+            try
+            {
+                context.Entry(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        //[Authorize(Roles = "manager")]
+        public async Task<ActionResult<User>> Post(
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                model.Role = "employee";
+
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+                model.Password = "";
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel criar o usuário" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate(
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            var user = await context.Users
+                .AsNoTracking()
+                .Where(x => x.UserName == model.UserName && x.Password == model.Password)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha invalidos" });
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+
+            return new
+            {
+                User = user,
+                token = token
+            };
+        }
+
+    }
+}
